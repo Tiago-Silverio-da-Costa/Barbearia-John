518e87e3fa2de09aac8d9623023f1f27eb598f6c
Tiago-Silverio-da-Costa <tiagosilveriodacosta@gmail.com>
2024-07-18 10:59:12 -0300

    text changed in about component

--- components/about.tsx
+++ components/about.tsx
@@ -63,17 +63,17 @@ export default function About() {
               sobre nós
             </p>
             <h1 className={`${didot.className} uppercase max-w-2xl text-goldColor leading-10 text-4xl font-bold mb-4`}>
-              Cuidando do seu estilo com dedicação, excelência e atenção aos detalhes desde 2017
+              Cuidando do seu estilo com excelência desde 2017
             </h1>
           </div>
           <p className="text-lg tracking-tighter leading-6 max-w-3xl">
-          Desde 2017, a Barbearia John está na ativa, firme e forte, cuidando do seu visual com todo carinho e dedicação. Somos uma barbearia tradicional e familiar que oferece cortes de cabelo com qualidade, atendimento personalizado e preço justo. 
-          <br />
-          <br />
-          Passamos por uns perrengues na pandemia, mudamos de lugar umas vezes, mas nunca desistimos. Agora, estamos aqui no <strong><Link aria-label="Barbearia John em Monte Alegre (Camboriú)" href="#contact" className={`text-goldColor`}>Monte Alegre, ao lado da escola CAIC, Camboriú</Link></strong>.
-          <br />
-          <br />
-          Queria agradecer de coração pela confiança ao longo desses anos todos. Se você ainda não veio conferir, venha conhecer a barbearia pra descobrir porque a galera escolhe a gente pra dar aquele tapa no visual e sair com todo estilo na rua!
+            Desde 2017, a Barbearia John está na ativa, firme e forte, cuidando do seu visual com todo carinho e dedicação. Somos uma barbearia tradicional e familiar que oferece cortes de cabelo com qualidade, atendimento personalizado e preço justo.
+            <br />
+            <br />
+            Passamos por uns perrengues na pandemia, mudamos de lugar umas vezes, mas nunca desistimos. Agora, estamos aqui no <strong><Link aria-label="Barbearia John em Monte Alegre (Camboriú)" href="#contact" className={`text-goldColor`}>Monte Alegre, ao lado da escola CAIC, Camboriú</Link></strong>.
+            <br />
+            <br />
+            Queria agradecer de coração pela confiança ao longo desses anos todos. Se você ainda não veio conferir, venha conhecer a barbearia pra descobrir porque a galera escolhe a gente pra dar aquele tapa no visual e sair com todo estilo na rua!
           </p>
         </div>
       </div>
